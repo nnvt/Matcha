e017c3a9063ae9ea5cef44b1c626af225bc467bc
Bach Nguyen <bachbet34@gmail.com>
2024-09-29 15:15:22 +0700

    update utils

---
+++ frontend/src/utils/customizeAxios.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 50,
+})
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8081/',
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    NProgress.start();
+    return config;
+}, function (error) {
+    // Do something with request error\
+    NProgress.start();
+    return Promise.reject(error);
+});
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    NProgress.done();
+    return response && response.data ? response.data : response;
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    // if (error.response.data && error.response.data.EC === -999) {
+    //     window.location.href = '/login'
+    // }
+
+    NProgress.done();
+    return error && error.response && error.response.data ? error.response.data : Promise.reject(error);
+});
+
+export default instance;
